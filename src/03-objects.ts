1e6ae0a1711da9ebddfe312fff15d3af1e747c52
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-12-03 14:56:43 +0300

    Array typing

--- src/03-objects.ts
+++ src/03-objects.ts
@@ -3,7 +3,7 @@
    для описания открытой ( public ) части объекта без реализации (api).
 */
 
-interface Car {
+interface ICar {
   wheels: number;
   brand: string;
   type: string;
@@ -13,14 +13,14 @@ interface Car {
   [key: string]: unknown; // неограниченное количество полей
 }
 
-const car: Car = {
+const car: ICar = {
   wheels: 4,
   brand: 'BMW',
   type: 'sedan',
   isNew: true,
 };
 
-const car2: Car = {
+const car2: ICar = {
   wheels: 4,
   brand: 'Ford',
   type: 'sedan',
---
+++ src/05-array.ts
@@ -0,0 +1,25 @@
+const array = [1, 2, 3, 4, 5];
+
+const arrayStrings: string[] = [];
+const arrayStrings2: Array<string> = [];
+
+//! arrayStrings2.push(3); будет ошибка
+arrayStrings2.push('newString');
+
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+const persons: Array<IPerson> = [];
+persons.push({ name: 'Tom', age: 15 });
+console.log(persons);
+
+const arrayOfArray: string[][] = [];
+arrayOfArray.push(['1', '2']);
+
+function printArray(array: unknown[]) {
+  array.forEach((el, index) => {
+    console.log(el, index);
+  });
+}
