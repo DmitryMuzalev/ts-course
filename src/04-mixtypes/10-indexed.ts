386c01a80e306f03ea576149d91a3de7b75aae06
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-12-12 14:58:01 +0300

    Access by index

---
+++ src/04-mixtypes/10-indexed.ts
@@ -0,0 +1,35 @@
+export {};
+
+//! Не рекомендуется так писать interface, лучше дробить по частям, т.е на маленькие interface или type
+
+interface DataModel {
+  id: number;
+  title: string;
+  elements: {
+    header: {
+      title: string;
+      description: string;
+      links: string[];
+    };
+    footer: {
+      description: string;
+      links: string[];
+    };
+    body: {
+      title: string;
+      content: {};
+    };
+  };
+}
+
+type T0 = DataModel["elements"]["footer"];
+
+type someTuple = [number, string, boolean, ...string[]];
+const arr: someTuple = [1, "str", true, "", ""];
+
+type T1 = someTuple[2]; // boolean
+type T2 = someTuple[3]; // string
+
+const sizes = ["small", "medium", "large"] as const; // readonly ["small", "medium", "large"]
+
+type T3 = (typeof sizes)[number]; // подучаем union "small" | "medium" | "large"
